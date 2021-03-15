4f0bd01c39ce439925a4aa8b25053056073120b3
UniqueDevStorm <storm@uniquecode.team>
2021-03-15 17:56:57 +0900

    feat: Main SYSTEM

---
+++ __main__.py
@@ -0,0 +1,23 @@
+from discord.ext import commands
+from discord import Intents
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv(verbose=True)
+TOKEN = os.getenv('TOKEN')
+
+
+class YourBOT(commands.Bot):
+    def __init__(self):
+        super().__init__(
+            command_prefix='!',
+            help_command=None,
+            intents=Intents.all()
+        )
+
+    async def on_ready(self):
+        print(f'{str(self.user)} on Ready.')
+
+
+YourBOT().run(TOKEN)
